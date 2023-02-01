c0db1e7add9b0f0aa0192410363d836c5a358991
rsjh4 <rsjh4@schls.albco>
2023-02-01 14:01:25 -0500

    creating new template for learners

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/SingleDriver.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import com.google.gson.internal.bind.util.ISO8601Utils;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//Welcome!
+
+
+@TeleOp(name ="Single Driver TeleOp")
+public class SingleDriver extends OpMode {
+    //Variable declarations
+
+    //Initiation process:
+    @Override
+    public void init () {
+        //HardwareMap ALL motors:
+    }
+
+    //Loop process: runs when play is pressed
+    @Override
+    public void loop () {
+
+    }
+}
