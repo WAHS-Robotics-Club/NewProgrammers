83bd66e94f1cffe414a16e4553d63dc6cf91214b
gssx3 <gssx3@schls.albco>
2023-02-08 17:18:40 -0500

    hello

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/AutonomousTemplate.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/AutonomousTemplate.java
@@ -1,7 +1,8 @@
 package org.firstinspires.ftc.robotcontroller.internal;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 //WELCOME!
 
 @Autonomous(name ="Autonomous")
