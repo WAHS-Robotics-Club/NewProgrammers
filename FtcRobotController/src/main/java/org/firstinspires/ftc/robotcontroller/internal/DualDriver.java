dea51986be41b16a936ecdede793d258da35b212
rsjh4 <rsjh4@schls.albco>
2023-02-07 21:07:10 -0500

    not important

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/DualDriver.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.util.Scanner;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@TeleOp(name ="Single Driver TeleOp")
+public class DualDriver extends OpMode {
+    DcMotor frontLeft, frontRight, backLeft, backRight;
+
+
+
+    @Override
+    public void init () {
+
+    }
+    @Override
+    public void loop () {
+
+    }
+}
