864c6668b3ee871a15e56f89a5b9bb279ca7d62b
rsjh4 <rsjh4@schls.albco>
2023-02-08 16:52:56 -0500

    help

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTemplate.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+//WELCOME!
+
+@Autonomous(name ="Autonomous")
+public class AutonomousTemplate extends LinearOpMode {
+    //variable declarations or methods
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //INIT PHASE BUTTON PRESSED
+        //HardwareMap DcMotors:
+        //create objects
+
+        //PLAY PHASE BUTTON PRESSED
+        //Wait for the button and subsequently wait 1/4 secs to start the program:
+        waitForStart();
+        sleep(250);
+
+        }
+    }
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriver.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//WELCOME!
+
+
+@TeleOp(name ="Single Driver TeleOp")
+public class SingleDriver extends OpMode {
+    //Variable declarations
+
+    //Initiation process:
+    @Override
+    public void init () {
+        //HardwareMap ALL motors:
+    }
+
+    //Loop process: runs when play is pressed
+    @Override
+    public void loop () {
+
+    }
+}
