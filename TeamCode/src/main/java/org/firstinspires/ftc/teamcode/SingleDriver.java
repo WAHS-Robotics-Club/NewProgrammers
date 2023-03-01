f8878c345bbfa5d7f8910ef351ebabb088416557
dqre6 <dqre6@schls.albco>
2023-03-01 16:07:09 -0500

    New Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriver.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+import com.qualcomm.robotcore.hardware.Servo;
 
 //WELCOME!
 
@@ -13,8 +13,8 @@ public class SingleDriver extends OpMode {
     //Variable declarations
     //trying this out
 
-    DcMotor frontLeft, frontRight, backLeft, backRight, spool, xButton;
-
+    DcMotor frontLeft, frontRight, backLeft, backRight, spool;
+    Servo grabber;
     //Initiation process:
     @Override
     public void init () {
@@ -24,7 +24,8 @@ public class SingleDriver extends OpMode {
         backLeft = hardwareMap.dcMotor.get("backLeft");
         backRight = hardwareMap.dcMotor.get("backRight");
         spool = hardwareMap.dcMotor.get("spool");
-        xButton = hardwareMap.dcMotor.get("xButton");
+        grabber = hardwareMap.servo.get("grabber");
+
 
 
 
@@ -36,28 +37,36 @@ public class SingleDriver extends OpMode {
 
         if(gamepad1.left_bumper) {
             frontLeft.setPower((-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x)/4);
-            frontRight.setPower((-gamepad1.left_stick_y + -gamepad1.left_stick_x + gamepad1.right_stick_x)/4);
+            frontRight.setPower((-gamepad1.left_stick_y -gamepad1.left_stick_x + gamepad1.right_stick_x)/4);
             backLeft.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x)/4);
-            backRight.setPower((gamepad1.left_stick_y + -gamepad1.left_stick_x + gamepad1.right_stick_x)/4);
+            backRight.setPower((gamepad1.left_stick_y -gamepad1.left_stick_x + gamepad1.right_stick_x)/4);
 
         }else {
             frontLeft.setPower((-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x));
-            frontRight.setPower((-gamepad1.left_stick_y + -gamepad1.left_stick_x + gamepad1.right_stick_x));
+            frontRight.setPower((-gamepad1.left_stick_y -gamepad1.left_stick_x + gamepad1.right_stick_x));
             backLeft.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x));
-            backRight.setPower((gamepad1.left_stick_y + -gamepad1.left_stick_x + gamepad1.right_stick_x));
+            backRight.setPower((gamepad1.left_stick_y -gamepad1.left_stick_x + gamepad1.right_stick_x));
         }
 
-        if(gamepad1.right_trigger > 0){
+
+        if(gamepad1.right_trigger > 0.01){
             spool.setPower(gamepad1.right_trigger);
         } else{
             spool.setPower(0);
         }
-
-        if(gamepad1.x){
-            xButton.setPower(0.75);
+        if(gamepad1.left_trigger > 0.01){
+            spool.setPower(-gamepad1.left_trigger);
         } else{
-            xButton.setPower(0);
+            spool.setPower(0);
         }
+
+        if(gamepad1.a){
+            grabber.setPosition((15) *(Math.PI/180));
+        } else if(gamepad1.x){
+            grabber.setPosition((30) *(Math.PI/180));
+        }
+
+
 
     }
 }
