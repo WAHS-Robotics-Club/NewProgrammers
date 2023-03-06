903e6b791b263fc0c5d343d9e6b819df6d0af157
dqre6 <dqre6@schls.albco>
2023-03-06 09:27:45 -0500

    For autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTemplate.java
@@ -1,25 +1,58 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 //WELCOME!
 
 @Autonomous(name ="Autonomous")
 public class AutonomousTemplate extends LinearOpMode {
     //variable declarations or methods
-
-
+DcMotor frontLeft, frontRight, backLeft, backRight;
+Servo grabber;
+double rotations;
+int targetPosition;
 
     @Override
     public void runOpMode() throws InterruptedException {
         //INIT PHASE BUTTON PRESSED
         //HardwareMap DcMotors:
         //create objects
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        grabber = hardwareMap.servo.get("grabber");
+
+
 
         //PLAY PHASE BUTTON PRESSED
         //Wait for the button and subsequently wait 1/4 secs to start the program:
         waitForStart();
         sleep(250);
+
+
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        rotations=(12/(4*Math.PI));
+        targetPosition = (int)(rotations*1120);
+
+        frontRight.setTargetPosition(targetPosition);
+        backRight.setTargetPosition(targetPosition);
+        frontLeft.setTargetPosition(-targetPosition);
+        frontLeft.setTargetPosition(-targetPosition);
+
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(0.8);
+
+
 
         }
     }
