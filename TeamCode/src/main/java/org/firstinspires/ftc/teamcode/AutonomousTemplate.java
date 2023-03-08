287a89b67a0db6f9b388ed5c273cf23221d0970f
mabm5 <mabm5@schls.albco>
2023-03-08 17:52:11 -0500

    howdy x 5

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTemplate.java
@@ -16,7 +16,7 @@ public class AutonomousTemplate extends LinearOpMode {
     double rotations;
     int targetPosition;
     int i = 0; //loops
-
+    BannanafFruit gyro = new BannanafFruit();
 
 
     @Override
@@ -33,7 +33,6 @@ public class AutonomousTemplate extends LinearOpMode {
         //create objects
 
 
-
         //PLAY PHASE BUTTON PRESSED
         //Wait for the button and subsequently wait 1/4 secs to start the program:
         waitForStart();
@@ -43,8 +42,8 @@ public class AutonomousTemplate extends LinearOpMode {
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         //12 is how far you movin yo
-        rotations = (12/(4.0*Math.PI)); //transfers inches into rotations (4 = rad * 2)
-        targetPosition = (int)(rotations*1120); // 1120 = roller wheel number
+        rotations = (12 / (4.0 * Math.PI)); //transfers inches into rotations (4 = rad * 2)
+        targetPosition = (int) (rotations * 1120); // 1120 = roller wheel number
         frontLeft.setTargetPosition(-targetPosition);
         frontRight.setTargetPosition(targetPosition);
         backLeft.setTargetPosition(-targetPosition);
@@ -64,18 +63,72 @@ public class AutonomousTemplate extends LinearOpMode {
         // && means that both things gotta be in action for IF to do it's command
         // || means that either or can do yes in IF
 
-        if (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()){
+        if (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
             isBusy = true;
         } else {
             isBusy = false;
         }
 
-        while((frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) && i < 500) {
+        while ((frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) && i < 500) {
 
             telemetry.update();
             i++;
             Thread.sleep(1);
         }
+        int targetHeading = 90;
+        // 90 is the degrees we turning
+        boolean isCorrectHeading = false;
+        int currentHeading;
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        while (!isCorrectHeading) {
+            telemetry.update();
+            currentHeading = gyro.getHeading();
+
+
+            if (targetHeading < gyro.getHeading() + 1.25 && targetHeading > gyro.getHeading() - 1.25) {
+                isCorrectHeading = true;
+            } else {
+                isCorrectHeading = false;
+            } // 1.25 is the margin of error
+            if (currentHeading > 145 || currentHeading < -145) {
+                if (currentHeading < 0) // less than 0 is uh. We don't know {
+                    currentHeading += 360; // adding 360 degrees?
+                // this fixes the robot from not doing full rotations when course adjusting!
+
+            }
         }
+
+        double modifier, basePower;
+        modifier = ((Math.sqrt(Math.abs(targetHeading - currentHeading))) / 2);
+        basePower = 0.1;
+        // change power with base power
+        // it's a sqrt function yo
+
+        if (targetHeading < currentHeading - 1.25) {
+            frontLeft.setPower(basePower * modifier);
+            frontRight.setPower(basePower * modifier);
+            backLeft.setPower(basePower * modifier);
+            backRight.setPower(basePower * modifier);
+        } else if ( targetHeading > currentHeading + 1.25){
+            frontLeft.setPower(-basePower * modifier);
+            frontRight.setPower(-basePower * modifier);
+            backLeft.setPower(-basePower * modifier);
+            backRight.setPower(-basePower * modifier);
+        }
+        else{
+            frontLeft.setPower(0);
+            frontRight.setPower(0);
+            backLeft.setPower(0);
+            backRight.setPower(0);
+            //adjusting mechanism yo
+
+        }
+        Thread.sleep(1);
     }
+}
 
