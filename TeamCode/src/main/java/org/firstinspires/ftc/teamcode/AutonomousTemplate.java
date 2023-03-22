2ec9de468808bd68b8889f3f41ad67e70dc54e6e
mabm5 <mabm5@schls.albco>
2023-03-22 17:17:52 -0400

    howey

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTemplate.java
@@ -31,6 +31,41 @@ public class AutonomousTemplate extends LinearOpMode {
         spool = hardwareMap.dcMotor.get("spool");
         grabber = hardwareMap.servo.get("grabber");
 
+        robot = new DriveTrain(frontLeft,frontRight,backLeft,backRight);
+
+        //Challenge #1
+        robot.Turn(90,telemetry,gyro);
+        robot.Drive(12, 0.8, telemetry);
+        robot.Turn(180,telemetry,gyro);
+        robot.Drive(12, 0.8, telemetry);
+        robot.Turn(270,telemetry,gyro);
+        robot.Drive(12, 0.8, telemetry);
+        robot.Turn(0,telemetry,gyro);
+        robot.Drive(12, 0.8, telemetry);
+
+        //Challenge #2
+        robot.Drive(16,0.8,telemetry);
+        robot.Turn(-90,telemetry,gyro);
+        robot.Drive(2,0.8,telemetry);
+        grabber.setPosition(30);
+        robot.Turn(90,telemetry,gyro);
+        robot.Drive(2,0.8,telemetry);
+        robot.Turn(180,telemetry,gyro);
+        robot.Drive(16,0.8,telemetry);
+
+        //Challenge #3?!?!?!?!
+        robot.Drive(12,0.8,telemetry);
+        spool.setPower(1);
+        sleep(2000);
+        spool.setPower(0);
+        robot.Turn(-90,telemetry,gyro);
+        robot.Drive(12,0.8, telemetry);
+        spool.setPower(-1);
+        sleep(2000);
+        spool.setPower(0);
+        robot.Turn(135,telemetry,gyro);
+        robot.Drive(17,0.8,telemetry);
+
         //create objects
 
 
@@ -39,61 +74,7 @@ public class AutonomousTemplate extends LinearOpMode {
         waitForStart();
         sleep(250);
 
-        robot.Drive(12,0.8,telemetry);
-        int targetHeading = 90;
-        // 90 is the degrees we turning
-        boolean isCorrectHeading = false;
-        int currentHeading;
-        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-
-        while (!isCorrectHeading) {
-            telemetry.update();
-            currentHeading = gyro.getHeading();
-
-
-            if (targetHeading < gyro.getHeading() + 1.25 && targetHeading > gyro.getHeading() - 1.25) {
-                isCorrectHeading = true;
-            } else {
-                isCorrectHeading = false;
-            } // 1.25 is the margin of error
-            if (currentHeading > 145 || currentHeading < -145) {
-                if (currentHeading < 0) // less than 0 is uh. We don't know {
-                    currentHeading += 360; // adding 360 degrees?
-                // this fixes the robot from not doing full rotations when course adjusting!
-
-            }
-        }
-
-        double modifier, basePower;
-        modifier = ((Math.sqrt(Math.abs(targetHeading - currentHeading))) / 2);
-        basePower = 0.1;
-        // change power with base power
-        // it's a sqrt function yo
-
-        if (targetHeading < currentHeading - 1.25) {
-            frontLeft.setPower(basePower * modifier);
-            frontRight.setPower(basePower * modifier);
-            backLeft.setPower(basePower * modifier);
-            backRight.setPower(basePower * modifier);
-        } else if ( targetHeading > currentHeading + 1.25){
-            frontLeft.setPower(-basePower * modifier);
-            frontRight.setPower(-basePower * modifier);
-            backLeft.setPower(-basePower * modifier);
-            backRight.setPower(-basePower * modifier);
-        }
-        else{
-            frontLeft.setPower(0);
-            frontRight.setPower(0);
-            backLeft.setPower(0);
-            backRight.setPower(0);
-            //adjusting mechanism yo
-
-        }
-        Thread.sleep(1);
     }
 }
 
