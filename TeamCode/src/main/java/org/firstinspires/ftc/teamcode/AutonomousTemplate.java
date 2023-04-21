42ea40e6c5e1e6992d2b2419d6305dfd6b2c6b37
Ryan <rsjh4@k12albemarle.org>
2023-04-21 15:33:08 -0400

    pushing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTemplate.java
@@ -19,7 +19,7 @@ public class AutonomousTemplate extends LinearOpMode {
     double rotations;
     int targetPosition;
     int i =0;
-    BananaFruit gyro = new BananaFruit();
+
 
 
 
@@ -34,6 +34,8 @@ public class AutonomousTemplate extends LinearOpMode {
         backRight = hardwareMap.dcMotor.get("backRight");
         spool = hardwareMap.dcMotor.get("spool");
         grabber = hardwareMap.servo.get("grabber");
+        BananaFruit gyro = new BananaFruit();
+        gyro.runBananaFruit(hardwareMap, telemetry);
 
 
 
